aeba19b0ee3deee03ab9f325d3da5025a4ec07b9
maliByatzes <malib2027@gmail.com>
2024-09-03 19:22:20 +0200

    construct a simple server

---
+++ http/server.go
@@ -0,0 +1,51 @@
+package http
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const TimeOut = 5 * time.Second
+
+type Server struct {
+	server *http.Server
+	router *gin.Engine
+	// services to used by various HTTP routes
+	// ...
+}
+
+func NewServer() *Server {
+	s := Server{
+		server: &http.Server{
+			WriteTimeout: TimeOut,
+			ReadTimeout:  TimeOut,
+			IdleTimeout:  TimeOut,
+		},
+		router: gin.Default(),
+	}
+
+	s.server.Handler = s.router
+
+	return &s
+}
+
+func (s *Server) Run(port string) error {
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+	s.server.Addr = port
+	log.Printf("🚀 Server starting on port %s", port)
+	return s.server.ListenAndServe()
+}
+
+func healthCheck() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "available",
+		})
+	}
+}
