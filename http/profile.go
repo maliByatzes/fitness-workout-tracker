72c046940302494339f57124f78274424b0fa6d9
maliByatzes <malib2027@gmail.com>
2024-09-12 08:29:26 +0200

    implement update profile handler

--- http/profile.go
+++ http/profile.go
@@ -91,3 +91,75 @@ func (s *Server) getUserProfile() gin.HandlerFunc {
 		})
 	}
 }
+
+func (s *Server) updateProfile() gin.HandlerFunc {
+	var req struct {
+		Profile struct {
+			FirstName   string    `json:"first_name"`
+			LastName    string    `json:"last_name"`
+			DateOfBirth time.Time `json:"dob"`
+			Gender      string    `json:"gender"`
+			Height      float64   `json:"height"`
+			Weight      float64   `json:"weight"`
+		} `json:"profile"`
+	}
+
+	return func(c *gin.Context) {
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		user := c.MustGet("user").(*fwt.User)
+		upd := fwt.ProfileUpdate{}
+		if req.Profile.FirstName != "" {
+			upd.FirstName = &req.Profile.FirstName
+		}
+		if req.Profile.LastName != "" {
+			upd.LastName = &req.Profile.LastName
+		}
+		if !req.Profile.DateOfBirth.IsZero() {
+			upd.DateOfBirth = &req.Profile.DateOfBirth
+		}
+		if req.Profile.Gender != "" {
+			upd.Gender = &req.Profile.Gender
+		}
+		if req.Profile.Height != 0 {
+			upd.Height = &req.Profile.Height
+		}
+		if req.Profile.Weight != 0 {
+			upd.Weight = &req.Profile.Weight
+		}
+
+		profile, err := s.ProfileService.FindProfileByUserID(c, user.ID)
+		if err != nil {
+			if fwt.ErrorCode(err) == fwt.ENOTFOUND && fwt.ErrorMessage(err) == "Profile not found." {
+				c.JSON(http.StatusNotFound, gin.H{
+					"error": fwt.ErrorMessage(err),
+				})
+				return
+			}
+			log.Printf("error in update profile handler: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal Server Error",
+			})
+			return
+		}
+
+		updatedProfile, err := s.ProfileService.UpdateProfile(c, profile.ID, upd)
+		if err != nil {
+			log.Printf("error in update profile handler: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal Server Error",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "profile updated successfully",
+			"profile": updatedProfile,
+		})
+	}
+}
