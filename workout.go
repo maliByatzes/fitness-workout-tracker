6ee5edb3c269a7fa02daf1e612849c10834af4d0
maliByatzes <malib2027@gmail.com>
2024-09-12 09:53:18 +0200

    scaffold workout service

---
+++ workout.go
@@ -0,0 +1,54 @@
+package fwt
+
+import (
+	"context"
+	"time"
+)
+
+type Workout struct {
+	ID            uint      `json:"id"`
+	UserID        uint      `json:"user_id"`
+	Name          string    `json:"name"`
+	ScheduledDate time.Time `json:"scheduled_date"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+func (w *Workout) Validate() error {
+	if w.UserID <= uint(0) {
+		return Errorf(EINVALID, "UserID is required.")
+	}
+
+	if w.Name == "" {
+		return Errorf(EINVALID, "Name is required.")
+	}
+
+	if w.ScheduledDate.IsZero() {
+		return Errorf(EINVALID, "Scheduled Date is required.")
+	}
+
+	return nil
+}
+
+type WorkoutService interface {
+	FindWorkoutByID(context.Context, uint) (*Workout, error)
+	FindWorkouts(context.Context, WorkoutFilter) ([]*Workout, int, error)
+	CreateWorkout(context.Context, *Workout) error
+	UpdateWorkout(context.Context, uint, WorkoutUpdate) (*Workout, error)
+	DeleteWorkout(context.Context, uint) error
+}
+
+type WorkoutFilter struct {
+	ID            *uint      `json:"id"`
+	UserID        *uint      `json:"user_id"`
+	Name          *string    `json:"name"`
+	ScheduledDate *time.Time `json:"scheduled_date"`
+
+	Offset int `json:"offset"`
+	Limit  int `json:"limit"`
+}
+
+type WorkoutUpdate struct {
+	Name          *string    `json:"name"`
+	ScheduledDate *time.Time `json:"scheduled_date"`
+}
